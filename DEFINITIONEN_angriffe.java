0a6846eecb0671bfe4ff9a17b93a67d611c5ace0
WolfBeat <vivianplonus@gmail.com>
2025-05-23 11:58:06 +0200

    Add files via upload

---
+++ DEFINITIONEN_angriffe.java
@@ -0,0 +1,10 @@
+class DEFINITIONEN_angriffe {
+    protected String Name;
+    protected Integer BP;
+    protected Integer Cost;
+    protected Integer Range;
+    protected Integer aoe;
+    protected Integer Element;
+    protected Integer SecEff;
+    protected Integer Acc;
+}
