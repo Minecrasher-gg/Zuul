7f7753d33dde9ae26b4862a8cad5fd93c1e08720
WolfBeat <vivianplonus@gmail.com>
2025-06-02 10:27:53 +0200

    Add files via upload

---
+++ DEFINITIONEN_Gegner.java
@@ -0,0 +1,28 @@
+class DEFINITIONEN_Gegner {
+    protected String Name;
+    protected Integer Rank;
+    protected Integer KiTyp;
+    protected Integer HP;
+    protected Integer MP;
+    protected Integer Atk;
+    protected Integer Def;
+    protected Integer MgAtk;
+    protected Integer MgDef;
+    protected Integer Eva;
+    protected Integer Acc;
+    protected Integer EXP;
+    public DEFINITIONEN_Gegner (String name, Integer rank, Integer kityp, Integer hp, Integer mp, Integer atk, Integer def, Integer mgatk, Integer mgdef, Integer eva, Integer acc, Integer exp){
+        name = Name;
+        rank = Rank;
+        kityp = KiTyp;
+        hp = HP;
+        mp = MP;
+        atk = Atk;
+        def = Def;
+        mgatk = MgAtk;
+        mgdef = MgDef;
+        eva = Eva;
+        acc = Acc;
+        exp = EXP;
+    }
+}
